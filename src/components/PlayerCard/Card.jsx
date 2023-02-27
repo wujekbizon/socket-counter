04e25422d7116b43c2b85e5982e1e913dddfc1ca
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-27 20:40:10 +0100

    adding more logic when gameover

--- src/components/PlayerCard/Card.jsx
+++ src/components/PlayerCard/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ playerName }) => {
   const [player_life, setPlayerLife] = useState(20);
 
   return (
-    <section className="card">
+    <section className={`${player_life === 0 && 'lost'} card`}>
       <div className="player_name">
         <h2>{playerName}</h2>
       </div>
@@ -14,14 +14,16 @@ const Card = ({ playerName }) => {
       </div>
       <div className="player_buttons">
         <button
-          onClick={() => setPlayerLife(player_life - 1)}
+          onClick={() => setPlayerLife(player_life <= 0 ? 0 : player_life - 1)}
           className="btn_danger"
+          disabled={player_life === 0}
         >
           <GiHeartMinus className="icon" />
         </button>
         <button
           onClick={() => setPlayerLife(player_life + 1)}
           className="btn_success"
+          disabled={player_life === 0}
         >
           <GiHeartPlus className="icon" />
         </button>
