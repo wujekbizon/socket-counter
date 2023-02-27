d292ee9377fd217ebc77ce791b46d3a4daae2da5
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-02-27 21:31:23 +0100

    creating Modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Modal = () => {
+  return <div className="modal">Modal</div>;
+};
+export default Modal;
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { ImMenu } from 'react-icons/im';
+import Modal from '../Modal/Modal';
 
 const Settings = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="settings">
-      <ImMenu className="menu_icon" onClick={() => setIsOpen(true)} />
-    </div>
+    <>
+      <div className="settings">
+        <ImMenu className="menu_icon" onClick={() => setIsOpen(true)} />
+      </div>
+      {isOpen && <Modal />}
+    </>
   );
 };
 export default Settings;
